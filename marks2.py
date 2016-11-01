ca6106a06451ac0063b33f816234468866ee3669
Taehee Jang <jang0913@gmail.com>
2016-11-01 23:18:37 +0900

    page 109

--- marks2.py
+++ marks2.py
@@ -1,2 +1,9 @@
+#-*- coding: utf-8 -*-
 # marks2.py
-marks = []
+marks = [90, 25, 67, 45, 80]
+
+number = 0
+for mark in marks:
+    number = number + 1
+    if mark < 60: continue
+    print "%d번 학생 축하합니다. 합격입니다." % number
